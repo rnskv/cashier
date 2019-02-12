df372ade99be23e9e5f2d25eac5b48d063350482
rnskv <web.rnskv@gmail.com>
2019-02-12 18:28:18 +0300

    Add default api route

--- src/server/router/index.js
+++ src/server/router/index.js
@@ -4,6 +4,10 @@ const router = express.Router();
 
 const userController = require('../controllers/user');
 
+router.get('/', (req, res) => {
+    res.send('Cashier api v1.0')
+});
+
 router.get('/user/register', userController.register);
 router.get('/user/login', userController.logIn);
 
