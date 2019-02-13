3192531cb85cd711705d19b76f8a010a37e27106
rnskv <web.rnskv@gmail.com>
2019-02-13 08:56:35 +0300

    Add redis database dependence

--- src/server/main.js
+++ src/server/main.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const redis = require('redis');
+const client = redis.createClient();
+
+
 const app = express();
 
 const useMainMiddlewares = require('./middlewares/main');
@@ -10,6 +14,19 @@ const User = require('./models/User');
 
 mongoose.connect(`mongodb://${config.db.user}:${config.db.password}@ds131765.mlab.com:31765/cashier`, {useNewUrlParser: true});
 
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+client.set("user", "shit", redis.print);
+client.get("user", function(err, reply) {
+    // reply is null when the key is missing
+    console.log(reply);
+});
+client.get("missingkey", function(err, reply) {
+    // reply is null when the key is missing
+    console.log(reply);
+});
 const player = new User({
     name: 'Roman',
     uid: 666,
